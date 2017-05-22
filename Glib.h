ce4a2c89c739c5fe3a8e35499ff3d5ba9f48f30a
PAM3ES <PAM3ES@ex.ua>
2017-05-22 19:09:15 +0300

    Update Glib.h

--- Glib.h
+++ Glib.h
@@ -18,6 +18,8 @@ const int UpWindow = 903;
 const int NONE = 404;
 const int GRectangle = 801;
 const int GLine = 802;
+const int GNFRectangle = 803;
+const int GArc = 804;
 const int CLICK = 1700;
 int StartButtonPress = 0;
 int Button[100000];
@@ -31,7 +33,7 @@ using Start=int;
  public:
      int Cord1[2];
      int Cord2[2];
-     XColor Color;
+      XColor Color;
      XFontStruct *TEXT;
      int red = 0; int green = 0; int blue = 0;
      int xC = 0; int yC = 0;
@@ -42,11 +44,7 @@ using Start=int;
      int ButtonPoint[10000];
      char fontname[128];
      
- void SetColor(int r, int g, int b) {
-     Color.flags = DoRed | DoGreen | DoBlue;
-     red , Color.red = r; green , Color.green = g; blue , Color.blue = b;
 
- }
  
  void DeclaringVariables(Window &w, GC &GraphContext, Colormap &cMap , int &s) {
          Point[5] = 25;
@@ -57,38 +55,43 @@ using Start=int;
 
      }
      
- void DrawItem(Display* d , Window w , int FunctionMode , int x1 , int y1, int x2, int y2) {
+ void DrawItem(Display* d , Window w,int r,int g,int b , int FunctionMode , int x1 , int y1, int x2, int y2 , int AdvanceParam = 0, int AdvanceParam2 = 0) {
          
     Cord1[0] = x1;
     Cord1[1] = y1;
     Cord2[0] = x2;
     Cord2[1] = y2;
+     Color.flags = DoRed | DoGreen | DoBlue;
+     red , Color.red = r; green , Color.green = g; blue , Color.blue = b;
             XAllocColor(d, *cMapClass , &Color);
             XSetForeground(d, *GraphContextClass, Color.pixel);
    if(FunctionMode == GRectangle) {
        XFillRectangle(d, w, *GraphContextClass, x1, y1, x2 , y2);
    }
-   
-   else if(FunctionMode == GLine) {
+   if(FunctionMode == GLine) {
        XDrawLine(d,w,*GraphContextClass,x1,y1,x2,y2);
    }
-     
+   if(FunctionMode == GNFRectangle) {
+        XDrawRectangle(d,w,*GraphContextClass,x1,y1,x2,y2);
+   }
+   if(FunctionMode == GArc) {
+        XFillArc(d,w,*GraphContextClass,x1,y1,x2,y2,AdvanceParam,AdvanceParam2);
+   }
+  Color.blue = 0; Color.green = 0; Color.red = 0;
  }
  
  void DrawText(Display* d,Window w, GC GraphContext, const char * text , int num , int x1 , int y1 , Colormap cMap , const char * font1 = "0") {
 
-          XAllocColor(d, cMap , &Color);
-          XSetForeground(d, GraphContext, Color.pixel);
-     if(font1 == "0")
-     {
+  XAllocColor(d, cMap , &Color);
+  XSetForeground(d, GraphContext, Color.pixel);
+     if(font1 == "0") {
         TEXT = XLoadQueryFont (d, "fixed");
      }
-     else
-     {
+     else {
         TEXT = XLoadQueryFont (d, font1);
      }
      XSetFont(d, GraphContext, TEXT->fid);
-          XDrawString (d, w, GraphContext, x1, y1, text, num);
+     XDrawString (d, w, GraphContext, x1, y1, text, num);
  }
  
  void UpdateWindow(Display *d, Window w,XEvent e) {
@@ -96,19 +99,17 @@ using Start=int;
      XSendEvent(d, w, True, 0, (XEvent *)&e);
  }
  
- 
 Display* OpenDisplay( const char* name) {
      
-    Display *d;
-         if ((d = XOpenDisplay(getenv(name))) == NULL) {
+   Display *d;
+    if ((d = XOpenDisplay(getenv(name))) == NULL) {
     printf("Can't connect X server:%s\n", strerror(errno));
     exit(1);
-    }
-    return d;
+   }
+  return d;
  }
  
  void DefScreen(Display *d , int &INTs) {
-     
      INTs = XDefaultScreen(d);
  }
  
@@ -153,16 +154,31 @@ public:
     int ButtonTF;
     int ButtonTrue;
     int ButtonFalse;
+     GC *GraphContextClassB;
+     Colormap *cMapClassB;
  void CreateButton(Display *d, Window w, GC gc2 , XEvent e ,int x1, int y1, int x2, int y2 ,int ModeFunction, int WhatReturnIfTrue, int WhatReturnIfFalse)
  {
   int xInFunc = 0; int yInFunc = 0;
-     XFillRectangle(d, w, gc2, x1, y1, x2-x1 , y2-y1);
+    XAllocColor(d, *cMapClassB , &Color);
+    XSetForeground(d, *GraphContextClassB, Color.pixel);
+    XFillRectangle(d, w, gc2, x1, y1, x2-x1 , y2-y1);
      CurrentPointerPressPos(e, xInFunc, yInFunc, WorkWithXY);
      
      if(xInFunc >= x1 && xInFunc <= x2 && yInFunc >= y1 && yInFunc <= y2) {ButtonTrue = WhatReturnIfTrue; ButtonTF = WhatReturnIfTrue; if(ModeFunction == UpWindow)
                                                                           {UpdateWindow(d,w,e);}                                      }
   else {ButtonFalse = WhatReturnIfFalse; ButtonTF = WhatReturnIfFalse;}
+StartButtonPress = 0;
+ }
+ 
+ void SetColorB(Display *d,int r,int g,int b) {
+     Color.flags = DoRed | DoGreen | DoBlue;
+     red , Color.red = r; green , Color.green = g; blue , Color.blue = b;
 
+ }
+ 
+ void NewButton(GC &GraphContext, Colormap &cMap) {
+    GraphContextClassB = &GraphContext;
+    cMapClassB = &cMap;
  }
 };
  
